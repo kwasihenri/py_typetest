3189ae923454bdf783cac18d60c73cee7e9ffe2e
kwasihenri <kwasihenri@gmail.com>
2025-12-05 11:54:06 +0000

    read sentences from file, provide final grade

--- wpm_tester.py
+++ wpm_tester.py
@@ -12,10 +12,15 @@ def main(stdscr):
         start_screen(stdscr)
         wpm, accuracy = wpm_test(stdscr)
         
-        stdscr.addstr(4, 0, "You completed the test!")
-        stdscr.addstr(5, 0, f"Final WPM: {wpm}")
-        stdscr.addstr(6, 0, f"Final Accuracy: {accuracy}%")
-        stdscr.addstr(8, 0, "Press 'p' to play again or any other key to exit.")
+        stdscr.clear()
+        result_string = (
+            "You completed the test!\n"
+            f"Final WPM: {wpm}\n"
+            f"Final Accuracy: {accuracy}%\n\n"
+            "Press 'p' to play again or any other key to exit."
+        )
+        stdscr.addstr(0, 0, result_string)
+        stdscr.refresh()
         
         key = stdscr.getkey()
         if key.lower() != 'p':
@@ -44,13 +49,17 @@ def display_text(stdscr, target, current, wpm=0):
 
 
 def load_text():
-    return random.choice([
-        "The quick brown fox jumps over the lazy dog.",
-        "Never underestimate the power of a good book.",
-        "The early bird catches the worm.",
-        "To be or not to be, that is the question.",
-        "The only thing we have to fear is fear itself."
-    ])
+    with open("test_sentences.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
+
+    # return random.choice([
+    #     "The quick brown fox jumps over the lazy dog.",
+    #     "Never underestimate the power of a good book.",
+    #     "The early bird catches the worm.",
+    #     "To be or not to be, that is the question.",
+    #     "The only thing we have to fear is fear itself."
+    # ])
 
 
 def wpm_test(stdscr):
@@ -68,7 +77,7 @@ def wpm_test(stdscr):
         display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
-        if "".join(current_text) == target_text:
+        if len(current_text) == len(target_text):
             stdscr.nodelay(False)
             break
 
@@ -86,13 +95,19 @@ def wpm_test(stdscr):
         elif len(current_text) < len(target_text):
             current_text.append(key)
             
+    total_time = max(time.time() - start_time, 1)
+    final_wpm = round((len(current_text) / (total_time / 60)) / 5)
+
     correct_chars = 0
     for i, char in enumerate(current_text):
         if char == target_text[i]:
             correct_chars += 1
-    accuracy = round((correct_chars / len(target_text)) * 100) if target_text else 0
     
-    return wpm, accuracy
+    accuracy = 0
+    if len(target_text) > 0:
+        accuracy = round((correct_chars / len(target_text)) * 100)
+
+    return final_wpm, accuracy
 
 
 wrapper(main)
